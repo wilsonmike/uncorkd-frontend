ca81af8e7467f4f9599edf43e3e9a08f8d2a7724
kris <kris.juncaj@gmail.com>
2020-12-08 14:26:45 -0500

    fixed search feature and api

---
+++ src/app/search-form/search-form.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-search-form',
+  templateUrl: './search-form.component.html',
+  styleUrls: ['./search-form.component.css'],
+})
+export class SearchFormComponent implements OnInit {
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {}
+
+  getSearchTerm = (form: NgForm): void => {
+    console.log(form.value);
+
+    this.router.navigate(['/home'], {
+      queryParams: {
+        term: form.value.search,
+      },
+    });
+  };
+}
