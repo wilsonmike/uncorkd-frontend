99dda4424c1b93d5b3699e003f401443d3f84750
Danielle Lucaj <danielle.lucaj@gmail.com>
2020-12-17 11:35:29 -0500

    almost there

--- src/app/submit-bourbon/submit-bourbon.component.ts
+++ src/app/submit-bourbon/submit-bourbon.component.ts
@@ -1,15 +1,50 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ServiceService } from '../service.service';
+import { AuthService } from '../services/auth/auth.service';
 
 @Component({
   selector: 'app-submit-bourbon',
   templateUrl: './submit-bourbon.component.html',
-  styleUrls: ['./submit-bourbon.component.css']
+  styleUrls: ['./submit-bourbon.component.css'],
 })
 export class SubmitBourbonComponent implements OnInit {
+  brand = '';
+  distillery = '';
+  proof = '';
+  upload_img = '';
+  description = '';
 
-  constructor() { }
+  constructor(
+    private service: ServiceService,
+    private authService: AuthService
+  ) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
+  getSubmission = (
+    brand: string,
+    distillery: string,
+    proof: number,
+    upload_img: any,
+    description: string
+  ): any => {
+    return this.service.getSubmission(
+      brand,
+      distillery,
+      proof,
+      upload_img,
+      description
+    );
+  };
+
+  storeSubmission = (form: NgForm) => {
+    console.log(form.value);
+    this.brand = form.value;
+    this.distillery = form.value;
+    this.proof = form.value;
+    this.upload_img = form.value;
+    this.description = form.value;
+    return form.value;
+  };
 }
